0b7c43498f15d4819f9e8f93b941786d5a06c0a5
SteffannyL <stefy_20152016@hotmail.com>
2026-03-16 03:34:51 -0500

    cambios2

--- frontend/src/app/features/coordinador/dashboard/dashboard-coordinador.component.ts
+++ frontend/src/app/features/coordinador/dashboard/dashboard-coordinador.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoordinadorService } from '../../../core/services/coordinador.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard-coordinador',
   standalone: true,
   imports:[CommonModule],
-  templateUrl: './dashboard-coordinador.component.html',
-  styleUrls: ['./dashboard-coordinador.component.css']
+  templateUrl:'./dashboard-coordinador.component.html',
+  styleUrls:['./dashboard-coordinador.component.css']
 })
 export class DashboardCoordinadorComponent implements OnInit {
 
@@ -15,7 +16,9 @@ export class DashboardCoordinadorComponent implements OnInit {
   aprobados = 0;
   rechazados = 0;
 
-  constructor(private coordinadorService:CoordinadorService){}
+  ultimosEventos:any[] = [];
+
+  constructor(private service:CoordinadorService){}
 
   ngOnInit(): void {
 
@@ -25,20 +28,56 @@ export class DashboardCoordinadorComponent implements OnInit {
 
   cargarDashboard(){
 
-    this.coordinadorService.getDashboard().subscribe({
+    this.service.getDashboard().subscribe({
 
       next:(data)=>{
-
-        console.log("Dashboard:",data);
 
         this.pendientes = data.pendientes;
         this.aprobados = data.aprobados;
         this.rechazados = data.rechazados;
 
+        this.crearGrafico();
+
+      }
+
+    });
+
+  }
+
+  crearGrafico(){
+
+    new Chart("graficoEventos",{
+      type:'doughnut',
+
+      data:{
+        labels:[
+          'Pendientes',
+          'Aprobados',
+          'Rechazados'
+        ],
+
+        datasets:[{
+          data:[
+            this.pendientes,
+            this.aprobados,
+            this.rechazados
+          ],
+
+          backgroundColor:[
+            '#f39c12',
+            '#27ae60',
+            '#c0392b'
+          ]
+        }]
       },
 
-      error:(err)=>{
-        console.error("Error cargando dashboard",err);
+      options:{
+        responsive:true,
+        plugins:{
+          legend:{
+            position:'bottom'
+          }
+        }
       }
 
     });
